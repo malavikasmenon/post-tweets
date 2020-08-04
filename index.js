4fdfd70903e0dab50e6598289c4cea2861563a05
malavikasmenon <malavikamenon2000@gmail.com>
2020-08-04 23:46:10 +0530

    Add package.json and index.js files.

---
+++ index.js
@@ -0,0 +1,22 @@
+let Twit = require('twit');
+let config = require('./config.js');
+const PromptSync = require('prompt-sync');
+let prompt = require('prompt-sync')();
+
+console.log("The bot is starting");
+
+let T = new Twit(config);
+
+let tweet = prompt('What do you want to sync?');
+
+
+T.post('statuses/update', { status: tweet }, tweeted);
+//defining the callback function
+function tweeted(err, data, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Success: ' + data.text);
+        //console.log(response);
+    }
+}
